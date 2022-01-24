f7e1e4f69626f1a4973ec104d1639eb6d70fb72c
Alex Osborne <aosborne@nla.gov.au>
2022-01-24 14:36:24 +0900

    Correct conditional hiding canonical link

--- src/views/general.nim
+++ src/views/general.nim
@@ -70,7 +70,7 @@ proc renderHead*(prefs: Prefs; cfg: Config; req: Request; titleText=""; desc="";
     link(rel="search", type="application/opensearchdescription+xml", title=cfg.title,
                             href=opensearchUrl)
 
-    if canonical.len > 0 or os.existsEnv("NLA_HIDE_NAVBAR"):
+    if canonical.len > 0 and not os.existsEnv("NLA_HIDE_NAVBAR"):
       link(rel="canonical", href=canonical)
 
     if cfg.enableRss and rss.len > 0:
